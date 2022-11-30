22358f21ddebc5a436a36f140c5a43cf4203cd0c
wenri0101 <98366068+wenri0101@users.noreply.github.com>
2022-11-30 15:08:26 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+function calcular(tipo, valor) {
+
+    if (tipo === 'acao') {
+        //bottões de ação
+
+
+        if (valor === 'C') {
+            //limpar o visor
+            document.getElementById('resultado').value = ''
+        }
+
+        if (valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.') {
+
+            document.getElementById('resultado').value += valor
+
+        }
+
+        if (valor === '=') {
+            var valor_campo = eval(document.getElementById('resultado').value)
+            document.getElementById('resultado').value = valor_campo
+
+        }
+
+
+    } else if (tipo === 'valor') {
+
+        //botões de valores
+        document.getElementById('resultado').value += valor
+
+    }
+
+}
